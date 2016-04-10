0519d646b37e7e2bb2be88a4970979f5a25ecfef
David Lu <davidelu93@gmail.com>
2016-04-10 01:06:07 -0400

    Immo.spec.js: add additional test for deeper inheritance

--- src/Immo.spec.js
+++ src/Immo.spec.js
@@ -82,3 +82,44 @@ test('inherited Immo', (t) => {
 
   t.end()
 })
+
+test('inherited*2 Immo', (t) => {
+  class ClassOne extends Immo(testProps, testState) {
+    foobar () {
+      return this.a + this.b
+    }
+  }
+
+  class ClassTwo extends ClassOne {
+    asdf () {
+      return this.b + this.c
+    }
+  }
+
+  let im1 = new ClassTwo(testProps, testState)
+  let im2 = im1.update({b: 'bar2', c: 'baz2'})
+  let im3 = im2.update({b: 'bar3', c: 'baz3'})
+
+  t.comment('im1')
+  t.equal(im1.a, 'foo')
+  t.equal(im1.b, 'bar')
+  t.equal(im1.c, 'baz')
+  t.equal(im1.foobar(), 'foobar')
+  t.equal(im1.asdf(), 'barbaz')
+
+  t.comment('im2')
+  t.equal(im2.a, 'foo')
+  t.equal(im2.b, 'bar2')
+  t.equal(im2.c, 'baz2')
+  t.equal(im2.foobar(), 'foobar2')
+  t.equal(im2.asdf(), 'bar2baz2')
+
+  t.comment('im3')
+  t.equal(im3.a, 'foo')
+  t.equal(im3.b, 'bar3')
+  t.equal(im3.c, 'baz3')
+  t.equal(im3.foobar(), 'foobar3')
+  t.equal(im3.asdf(), 'bar3baz3')
+
+  t.end()
+})
