70c5bcb02b8d532fd01702d4a4241e801c7f48af
David Lu <davidelu93@gmail.com>
2016-04-27 00:32:27 -0400

    adds computed properties to Immo + implements state change handling thru `updateIfOutdated`

--- src/Immo.js
+++ src/Immo.js
@@ -2,6 +2,7 @@
  * Immo: Immutable Manually Managed Object (I guess?)
  * props: immutable properties. these stay the same
  * state: immutable state. these get updated
+ * computed: mutable derived data
  *
  * usage:
  * - class MyClass extends Immo(propKeys, stateKeys) {}
@@ -12,37 +13,53 @@
  *
  */
 
-function defineProps (obj, keys, getPropsKey) {
+function defineProps (obj, keys, getPropsKey, setPropsKey) {
+  if (!keys) return
   for (let key of keys) {
     Object.defineProperty(obj, key, {
       // enumerable: true,
-      get: getPropsKey(key)
+      get: getPropsKey(key),
+      set: setPropsKey && setPropsKey(key)
     })
   }
 }
 
-export default function Immo (propsKeys, stateKeys) {
-  if (!(propsKeys instanceof Array)) {
-    propsKeys = Object.keys(propsKeys)
-  }
-  if (!(stateKeys instanceof Array)) {
-    stateKeys = Object.keys(stateKeys)
-  }
+export default function Immo (...keys) {
+  keys = keys.map((keys) => (keys instanceof Array) ? keys : Object.keys(keys))
+  let [propsKeys, stateKeys, computedKeys] = keys
   class ImmoClass {
-    constructor (props, state) {
-      Object.defineProperties(this, {
-        _props: { value: props },
-        _state: { value: state },
-        _init: { value: this }
-      })
+    constructor (props, state, computed) {
+      this.__init__ = this
+      this.__props__ = props
+      this.__state__ = state
+      this.__computed__ = computed
+      this.__current__ = this
     }
-    update (nextState) {
-      return Object.create(this._init, {
-        _state: { value: nextState }
+    updateIfOutdated (updateFns) {
+      let current = this.__current__
+      if (this !== current) {
+        this.__init__.__current__ = this
+        for (let stateKey in updateFns) {
+          if (current[stateKey] !== this[stateKey]) {
+            updateFns[stateKey](current)
+          }
+        }
+      }
+    }
+    update (updated) {
+      let next = Object.create(this.__init__, {
+        __state__: { value: Object.assign({}, this.__state__, updated) }
       })
+      this.__init__.__current__ = next
+      return next
     }
   }
-  defineProps(ImmoClass.prototype, propsKeys, (key) => function () { return this._props[key] })
-  defineProps(ImmoClass.prototype, stateKeys, (key) => function () { return this._state[key] })
+  let defineImmoProps = defineProps.bind(null, ImmoClass.prototype)
+  defineImmoProps(propsKeys, (key) => function () { return this.__props__[key] })
+  defineImmoProps(stateKeys, (key) => function () { return this.__state__[key] })
+  defineImmoProps(computedKeys,
+    (key) => function () { return this.__computed__[key] },
+    (key) => function (value) { this.__computed__[key] = value }
+  )
   return ImmoClass
 }
--- src/Immo.spec.js
+++ src/Immo.spec.js
@@ -4,87 +4,130 @@ import Immo from './Immo.js'
 
 let testProps = {a: 'foo'}
 let testState = {b: 'bar', c: 'baz'}
+let testComputed = {d: 42}
 
 test('plain Immo from array', (t) => {
-  let Im = Immo(['a'], ['b', 'c'])
-  let im = new Im(testProps, testState)
+  let Im = Immo(['a'], ['b', 'c'], ['d'])
+  let im = new Im(testProps, testState, testComputed)
 
   t.equal(im.a, 'foo')
   t.equal(im.b, 'bar')
   t.equal(im.c, 'baz')
+  t.equal(im.d, 42)
 
   t.end()
 })
 
 test('plain Immo from object', (t) => {
-  let Im = Immo(testProps, testState)
-  let im = new Im(testProps, testState)
+  let Im = Immo(testProps, testState, testComputed)
+  let im = new Im(testProps, testState, testComputed)
 
   t.equal(im.a, 'foo')
   t.equal(im.b, 'bar')
   t.equal(im.c, 'baz')
+  t.equal(im.d, 42)
 
   t.end()
 })
 
+function testVersioning (t, im1, im2) {
+  t.test('versions', (t) => {
+    t.plan(4)
+    im2.updateIfOutdated({
+      b () {
+        t.fail('im3 should not be outdated')
+      }
+    })
+    im1.updateIfOutdated({
+      b (im) {
+        t.pass('im1 is outdated')
+        t.equal(im, im2, 'current version is im3')
+      }
+    })
+    im1.updateIfOutdated({
+      c () {
+        t.fail('im1 should not be outdated')
+      }
+    })
+    im2.updateIfOutdated({
+      c (im) {
+        t.pass('im3 is outdated')
+        t.equal(im, im1, 'current version is im1')
+      }
+    })
+  })
+}
+
 test('plain Immo update', (t) => {
-  let Im = Immo(testProps, testState)
-  let im1 = new Im(testProps, testState)
+  let Im = Immo(testProps, testState, testComputed)
+  let im1 = new Im(testProps, testState, testComputed)
   let im2 = im1.update({b: 'bar2', c: 'baz2'})
   let im3 = im2.update({c: 'baz3'})
+  im3.d = 43
 
   t.comment('im1')
   t.equal(im1.a, 'foo')
   t.equal(im1.b, 'bar')
   t.equal(im1.c, 'baz')
+  t.equal(im1.d, 43)
 
   t.comment('im2')
   t.equal(im2.a, 'foo')
   t.equal(im2.b, 'bar2')
   t.equal(im2.c, 'baz2')
+  t.equal(im2.d, 43)
 
   t.comment('im3')
   t.equal(im3.a, 'foo')
-  t.equal(im3.b, undefined) // b is not assigned!
+  t.equal(im3.b, 'bar2')
   t.equal(im3.c, 'baz3')
+  t.equal(im3.d, 43)
+
+  testVersioning(t, im1, im3)
 
   t.end()
 })
 
 test('inherited Immo', (t) => {
-  class MyClass extends Immo(testProps, testState) {
+  class MyClass extends Immo(testProps, testState, testComputed) {
     foobar () {
       return this.a + this.b
     }
   }
 
-  let im1 = new MyClass(testProps, testState)
+  let im1 = new MyClass(testProps, testState, testComputed)
   let im2 = im1.update({b: 'bar2', c: 'baz2'})
   let im3 = im2.update({b: 'bar3', c: 'baz3'})
+  im3.d = 43
 
   t.comment('im1')
   t.equal(im1.a, 'foo')
   t.equal(im1.b, 'bar')
   t.equal(im1.c, 'baz')
   t.equal(im1.foobar(), 'foobar')
+  t.equal(im1.d, 43)
 
   t.comment('im2')
   t.equal(im2.a, 'foo')
   t.equal(im2.b, 'bar2')
   t.equal(im2.c, 'baz2')
   t.equal(im2.foobar(), 'foobar2')
+  t.equal(im2.d, 43)
 
   t.comment('im3')
   t.equal(im3.a, 'foo')
   t.equal(im3.b, 'bar3')
   t.equal(im3.c, 'baz3')
   t.equal(im3.foobar(), 'foobar3')
+  t.equal(im3.d, 43)
+
+  testVersioning(t, im1, im3)
 
   t.end()
 })
 
 test('inherited*2 Immo', (t) => {
-  class ClassOne extends Immo(testProps, testState) {
+  class ClassOne extends Immo(testProps, testState, testComputed) {
     foobar () {
       return this.a + this.b
     }
@@ -96,9 +139,10 @@ test('inherited*2 Immo', (t) => {
     }
   }
 
-  let im1 = new ClassTwo(testProps, testState)
+  let im1 = new ClassTwo(testProps, testState, testComputed)
   let im2 = im1.update({b: 'bar2', c: 'baz2'})
   let im3 = im2.update({b: 'bar3', c: 'baz3'})
+  im3.d = 43
 
   t.comment('im1')
   t.equal(im1.a, 'foo')
@@ -106,6 +150,7 @@ test('inherited*2 Immo', (t) => {
   t.equal(im1.c, 'baz')
   t.equal(im1.foobar(), 'foobar')
   t.equal(im1.asdf(), 'barbaz')
+  t.equal(im1.d, 43)
 
   t.comment('im2')
   t.equal(im2.a, 'foo')
@@ -113,6 +158,7 @@ test('inherited*2 Immo', (t) => {
   t.equal(im2.c, 'baz2')
   t.equal(im2.foobar(), 'foobar2')
   t.equal(im2.asdf(), 'bar2baz2')
+  t.equal(im2.d, 43)
 
   t.comment('im3')
   t.equal(im3.a, 'foo')
@@ -120,6 +166,9 @@ test('inherited*2 Immo', (t) => {
   t.equal(im3.c, 'baz3')
   t.equal(im3.foobar(), 'foobar3')
   t.equal(im3.asdf(), 'bar3baz3')
+  t.equal(im3.d, 43)
+
+  testVersioning(t, im1, im3)
 
   t.end()
 })
