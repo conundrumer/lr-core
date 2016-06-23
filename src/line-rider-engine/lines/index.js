774af6045efeb334136cc1c68492a4b5eff2af21
David Lu <davidelu93@gmail.com>
2016-06-22 23:23:26 -0400

    fix bug with line extensions not getting loaded

--- src/line-rider-engine/lines/index.js
+++ src/line-rider-engine/lines/index.js
@@ -4,7 +4,15 @@ import AccLine from './AccLine.js'
 import SceneryLine from './SceneryLine.js'
 
 export {default as LineTypes} from './LineTypes.js'
+
+const LEFT_EXTENDED = 1
+const RIGHT_EXTENDED = 2
+
 export function createLineFromJson (data) {
+  if (data.extended) {
+    data.leftExtended = !!(LEFT_EXTENDED & data.extended)
+    data.rightExtended = !!(RIGHT_EXTENDED & data.extended)
+  }
   switch (data.type) {
     case undefined:
       throw new TypeError(`Line JSON requires type: ${data.toString()}`)
